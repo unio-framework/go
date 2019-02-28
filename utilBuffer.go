ba4936d1d8f9acdd0e9e848b9b71f0ee0d57b73b
Leandro Takagi <leakira.uac@gmail.com>
2019-02-28 15:14:17 -0300

    Init

---
+++ utilBuffer.go
@@ -0,0 +1,29 @@
+package unio
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/labstack/echo"
+	"io/ioutil"
+)
+
+func GetBuffer(c echo.Context) (buffer []byte) {
+	buffer, err := ioutil.ReadAll(c.Request().Body)
+	if err != nil {
+		TraceError(err)
+	}
+	ResetBuffer(c, buffer)
+	return
+}
+
+func ResetBuffer(c echo.Context, buffer []byte) {
+	c.Request().Body = ioutil.NopCloser(bytes.NewBuffer(buffer))
+}
+
+func InterfaceToBuffer(data interface{}) []byte {
+	buffer, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+	return buffer
+}
