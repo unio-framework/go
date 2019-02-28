c2b8ca13429a90ece887d5df097e450b40ed81ab
Leandro Takagi <leakira.uac@gmail.com>
2019-02-28 15:15:40 -0300

    Init

---
+++ utilResponse.go
@@ -0,0 +1,32 @@
+package unio
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/gommon/log"
+	"net/http"
+)
+
+func RequestResult(c echo.Context, status int, data interface{}, errors interface{}) error {
+	result := echo.Map{
+		"status": status,
+	}
+
+	if status == http.StatusOK {
+		result["status"] = 1
+		result["data"] = data
+		if errors != nil {
+			result["error_message"] = errors
+		}
+	} else {
+		result["status"] = 0
+		result["error_message"] = data
+	}
+
+	return c.JSON(status, result)
+}
+
+func TraceError(err error) {
+	if err != nil {
+		log.Error(err)
+	}
+}
