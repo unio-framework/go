2d5223a6b3daf402f11d2f2e792cc7281dce6c0c
Leandro Takagi <leakira.uac@gmail.com>
2019-04-12 18:25:11 -0300

    [0.2.0]

--- utilConvert.go
+++ utilConvert.go
@@ -4,21 +4,10 @@ import (
     "encoding/json"
     "errors"
     "github.com/labstack/gommon/log"
+    "gopkg.in/mgo.v2/bson"
     "strconv"
     "time"
 )
-
-// Validate if value is string, if is, try to convert to int. If error, returns -999
-func (u *Util) StringToInt(value interface{}) (number int, err error) {
-    number = -999
-    if s,ok := value.(string); ok {
-        res, err := strconv.Atoi(s)
-        if err == nil { number = res }
-    } else {
-        err = errors.New("variable is not string")
-    }
-    return number, err
-}
 
 // Validate if value is string, if is, try to convert to float/32. If error, returns -999
 func (u *Util) StringToFloat(value interface{}) (number float32, err error) {
@@ -32,15 +21,16 @@ func (u *Util) StringToFloat(value interface{}) (number float32, err error) {
     return number, err
 }
 
-// Validate if value is string and try to convert one time format to another format
-func (u *Util) TimeConvert(value interface{}, from string, to string) (result string, err error) {
-    t, err := time.Parse(from, value.(string))
-    if err == nil {
-        return t.Format(to), nil
+// Validate if value is string, if is, try to convert to int. If error, returns -999
+func (u *Util) StringToInt(value interface{}) (number int, err error) {
+    number = -999
+    if s,ok := value.(string); ok {
+        res, err := strconv.Atoi(s)
+        if err == nil { number = res }
     } else {
-        log.Error(err)
+        err = errors.New("variable is not string")
     }
-    return "", err
+    return number, err
 }
 
 // Convert struct to string key map
@@ -52,4 +42,25 @@ func (u *Util) StructToMap(data interface{}) (map[string]interface{}, error) {
 
     err = json.Unmarshal(b, &result)
     return result, err
+}
+
+// Validate if value is string and try to convert one time format to another format
+func (u *Util) TimeConvert(value interface{}, from string, to string) (result string, err error) {
+    t, err := time.Parse(from, value.(string))
+    if err == nil {
+        return t.Format(to), nil
+    } else {
+        log.Error(err)
+    }
+    return "", err
+}
+
+// Validate an interface{} var and return an MongoDB ObjectId
+func (u *Util) ToObjectId(id interface{}) bson.ObjectId {
+    if hexId, ok := id.(bson.ObjectId); ok {
+        return hexId
+    } else if stringId, ok := id.(string); ok && bson.IsObjectIdHex(stringId) {
+        return bson.ObjectIdHex(stringId)
+    }
+    return ""
 }
